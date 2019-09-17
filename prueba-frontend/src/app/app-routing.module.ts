b3039181566a35153c7dd4d14095bcf2d88eb2c0
jearias <jearias@stefanini.com>
2019-09-16 23:47:31 -0500

    Resololución preguntas, actualización usuario persistencia datos JSON

--- prueba-frontend/src/app/app-routing.module.ts
+++ prueba-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FaqComponent } from './faq/faq.component';
 
 const routes: Routes = [
+  { path: 'faq', component: FaqComponent},
   { path: 'players', loadChildren: './players/players.module#PlayersModule'}
 ];
 
